4c2a6e295d6d064406901fe64e6abe0ec1b80279
Mayur Patil <mayurvij22@gmail.com>
2025-11-14 23:19:39 +0530

    doe

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ app.use(express.json());
 
 app.use("/api/email", emailRoutes);
 
+console.log("Server is starting...");
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
