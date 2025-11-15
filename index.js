dddfbbaaa6d33e3513ff6790394ecca61ed98300
Mayur Patil <mayurvij22@gmail.com>
2025-11-15 15:21:06 +0530

    d2

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import emailRoutes from "./routes/emailRoutes.js";
-
-dotenv.config();
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/email", emailRoutes);
-
-console.log("Server is starting...");
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+console.log("Hello, World!");
